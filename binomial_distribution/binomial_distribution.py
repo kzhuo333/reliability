2d57aaa59561a7a4ef7d08109b055f6c94037cc1
kzhuo333 <kzhuo3@gmail.com>
2023-04-07 15:03:35 -0700

    Added sample size textbox. Rearrange plots and adjust positions. Working on plot updating.

--- binomial_distribution/binomial_distribution.py
+++ binomial_distribution/binomial_distribution.py
@@ -1,21 +1,28 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
 import seaborn as sns
-from typing import Final
+from typing import Final, Union
 from scipy.stats import binom
 
 # Binomial distribution default parameters
-SAMPLE_SIZE: Final[int] = 1000
-PFAIL: Final[int] = 0.01
+SAMPLE_SIZE: Final[int] = 100
+PFAIL: Final[int] = 0.05
 
-# Plot position default settings
-LEFT:Final[float] = 0.1
-TOP:Final[float] = 0.75
-WSPACE:Final[float] = 0.2
+# Overall canvas size
+FIG_HEIGHT:Final[float] = 8.0
+FIG_WIDTH:Final[float] = 7.0
+# Plot position default settings (do not use tight_layout with these)
+# These are margins within the figure canvas
+LEFT:Final[float] = 0.2
+TOP:Final[float] = 0.8
+# Padding between subplots
+#WSPACE:Final[float] = 0.2
 HSPACE:Final[float] = 0.5
-FIG_HEIGHT:Final[float] = 6.0
-FIG_WIDTH:Final[float] = 9.0
+
+# General tolerance value
+TOL:Final[float] = 1E-6
 
 class binomial_model:
     def __init__(self, n: int = SAMPLE_SIZE, pfail: float = PFAIL)->None:
@@ -25,28 +32,46 @@ class binomial_model:
         self.reset_model()
 
     def reset_model(self)->None:
+        # Not sure how many elements to include in each data array so initialise each with the max possible length
         # Set up horizontal axis data of integer number of fails
         self.x_data = np.arange(0, self.n+1)
-
         # Set up vertical axes data of probability of getting x number of fails
         self.pmf_data = np.empty(len(self.x_data))
         self.cdf_data = np.empty(len(self.x_data))
-        for i, c in enumerate(self.x_data):
+
+        for c in np.arange(0, self.n+1):
+            # Set horizontal axis point
+            self.x_data[c] = c
             # apply prob dens function
-            self.pmf_data[i] = binom.pmf(c, self.n, self.pfail)
+            self.pmf_data[c] = binom.pmf(c, self.n, self.pfail)
             # apply cumulative density function
-            self.cdf_data[i] = binom.cdf(c, self.n, self.pfail)
+            self.cdf_data[c] = binom.cdf(c, self.n, self.pfail)
+
+            # Break the loop once the CDF gets close enough to its max allowed value of 1.0
+            if self.cdf_data[c] > 1-TOL:
+                break
+
+        # Now we know number of elements needed, cut off elements beyond that
+        self.x_data = self.x_data[0:c+1]
+        self.pmf_data = self.pmf_data[0:c+1]
+        self.cdf_data = self.cdf_data[0:c+1]
+
+    def update_pfail(self, pfail:float)->None:
+        self.pfail = pfail
+
+    def update_n(self, n:int)->None:
+        self.n = n
 
 class binomial_plot:
     def __init__(self, model:binomial_model)->None:
         self.model = model
 
         # Default main plot settings and definitions
-        self.fig, (self.ax0, self.ax1) = plt.subplots(1,2)
+        self.fig, ((self.ax0), (self.ax1)) = plt.subplots(2,1)
         self.fig.set_figheight(FIG_HEIGHT)
         self.fig.set_figwidth(FIG_WIDTH)
         self.fig.suptitle("Binomial Distribution", weight="bold")
-        self.fig.subplots_adjust(left=LEFT, top=TOP, wspace = WSPACE, hspace=HSPACE)
+        self.fig.subplots_adjust(left=LEFT, top=TOP, hspace = HSPACE)
 
         # Generate plots
         # Note this must be done before setting axis properties, or else will be a mess
@@ -58,23 +83,91 @@ class binomial_plot:
         self.ax1.set_title("Cumulative Density Function", weight="bold")
         
         # Plot axis labels and grid
-        self.ax0.set_ylabel("Probability Density")
-        self.ax1.set_ylabel("Cumulative Probability Density")        
+        self.ax0.set_ylabel("Probability")
+        self.ax1.set_ylabel("Cumulative Probability")        
         self.ax0.set_ylim(auto=True)
         self.ax1.set_ylim(top = 1.0)
 
-        
-        xlim = self.get_xlim()
         for x in [self.ax0, self.ax1]:
-            x.set_xlim(left = 0.0, right = xlim)
             x.set_xlabel("Number Of Fails")
-            x.grid(visible=True, which='both', axis='both')
+            x.set_xticklabels(x.get_xticklabels(), rotation=45)
+            x.grid(visible=True, which='both', axis='y')
+            x.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(5))
+            x.xaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
 
-    def get_xlim(self):
-        default_index = len(self.model.cdf_data) - 1 
-        idx = next((i for i, xx in enumerate(self.model.cdf_data) if xx > 1-1E-9), default_index)
-        return self.model.x_data[idx]
+        self.make_pfail_tbox()
+        self.make_n_tbox()
+
+    def update_data(self)->None:
+        self.model.reset_model()
+
+        sns.barplot(x=self.model.x_data, y=self.model.pmf_data, ax=self.ax0, color = 'Orange')
+        sns.barplot(x=self.model.x_data, y=self.model.cdf_data, ax=self.ax1, color = 'Green')
+
+        # Rescale the axes where scaling is set to auto
+        for x in [self.ax0, self.ax1]:
+            x.relim()
+            x.autoscale_view()
+        
+        self.fig.canvas.draw() # This is needed to force the plot to refresh or there will be some delay
+        plt.show()
+
+    def make_pfail_tbox(self)->None:
+        
+        pfail_tbox_ax = self.fig.add_axes([0.3, 0.9, 0.075, 0.04])
+        self.pfail_tbox = TextBox(pfail_tbox_ax, "Probability Of Failure", textalignment="left")
+        self.pfail_tbox.set_val(f"{PFAIL}")
+        # Hook up update method to the event
+        self.pfail_tbox.on_submit(self.pfail_update)
+
+    def pfail_update(self, val:Union[str,int])->None:
+        
+        try:
+            pfail = float(val)
+        except ValueError:
+            print("Invalid probability of failure")
+            return
+        
+        # Prob of failure must be between 0.0 and 1.0
+        if pfail > 1.0:
+            print("Probability of failure must be less than 1.0")
+            return
+        elif pfail < 0.0:
+            print("Probability of failure must be more than 0.0")
+            return
+
+        print(f"New pfail {pfail}")
+        self.model.update_pfail(pfail)
+        self.update_data()
+
+    def make_n_tbox(self)->None:
+        
+        pfail_n_ax = self.fig.add_axes([0.6, 0.9, 0.075, 0.04])
+        self.n_tbox = TextBox(pfail_n_ax, "Sample Size", textalignment="left")
+        self.n_tbox.set_val(f"{SAMPLE_SIZE}")
+        # Hook up update method to the event
+        self.n_tbox.on_submit(self.n_update)
+
+    def n_update(self, val:Union[str,int])->None:
+        
+        try:
+            n = int(val)
+        except ValueError:
+            print("Invalid sample size n")
+            return
+        
+        # Prob of failure must be between 0.0 and 1.0
+        if n > 10000:
+            print("Sample size must be less than 10000")
+            return
+        elif n < 1:
+            print("Sample size must be more than 0")
+            return
+
+        print(f"Sample size {n}")
+        self.model.update_n(n)
+        self.update_data()        
 
 if __name__ == '__main__':
     # Set the matplotlib plotting backend
@@ -84,7 +177,9 @@ if __name__ == '__main__':
     print(f"Using backend {matplotlib.get_backend()}")
 
     m = binomial_model()
+    print(m.x_data)
+    print(m.pmf_data)
+
     p = binomial_plot(m)
 
-    plt.tight_layout()
     plt.show()
